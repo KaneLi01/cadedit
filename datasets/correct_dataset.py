5bffbfbc5dd2af4da686ecf010a2998e68d4bdee
KaneLi01 <dstny6102@gmail.com>
2025-05-20 16:57:45 +0800

    修正数据集

--- datasets/correct_dataset.py
+++ datasets/correct_dataset.py
@@ -3,21 +3,68 @@ sys.path.append("..")
 import os
 import cv2
 import numpy as np
-from vis.vis_utils import get_obj_contour_from_img
+
 
 '''将制作的初始数据集进行修正'''
 
-def correct_sketch_img(sketch_img_path, align_normal_img_path):
-    '''将初始渲染的sketch图像 尽可能贴合到渲染的normla图像上'''
+def get_obj_paras(img):
+    '''需要输入白色背景的图片'''
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, binary = cv2.threshold(gray, 254, 255, cv2.THRESH_BINARY_INV)
 
-    pass
+    points = cv2.findNonZero(binary)
+    if points is not None:
+        x, y, w, h = cv2.boundingRect(points)
+        return x, y, w, h
+    else: return None
+
+
+def correct_sketch_img(sketch_img_path, align_normal_img_path, output_path):
+    '''将初始渲染的sketch图像 尽可能贴合到渲染的normla图像上'''
+    sketch_img = cv2.imread(sketch_img_path)  
+    align_img = cv2.imread(align_normal_img_path)
+    if sketch_img is None or align_img is None:
+        raise FileNotFoundError("图片读取失败")
+    sx, sy, sw, sh = get_obj_paras(sketch_img)
+    ax, ay, aw, ah = get_obj_paras(align_img)
+
+    roi2 = sketch_img[sy:sy+sh, sx:sx+sw]  # 裁减
+    resized_roi2 = cv2.resize(roi2, (aw, ah), interpolation=cv2.INTER_LINEAR)  # 缩放
+    result = np.full_like(align_img, 255)
+    result[ay:ay+ah, ax:ax+aw] = resized_roi2
+
+    cv2.imwrite(output_path, result)
+
+
+
 
 def correct_sketch_dataset():
     '''修正sketch数据集'''
-    sketch_img_path = '/home/lkh/siga/dataset/my_dataset/normals_train_dataset/init_sketch/00017021/0.png'
-    align_normal_img_path = '/home/lkh/siga/output/test/correct/bg/result.png'
-    output_path = '/home/lkh/siga/output/test/correct/sketch'
-    correct_sketch_img(sketch_img_path, align_normal_img_path)
+    sketch_dir = '/home/lkh/siga/dataset/my_dataset/normals_train_dataset/init_img_sketch/'
+    align_normal_img_dir = '/home/lkh/siga/dataset/my_dataset/normals_train_dataset/normal_img_addbody/operate/'
+    output_dir = '/home/lkh/siga/dataset/my_dataset/normals_train_dataset/img_sketch'
+    type_name = os.listdir(sketch_dir)
+    for t in type_name:
+        os.makedirs(os.path.join(output_dir, t), exist_ok=True)
+        sketch_img_dir = os.path.join(sketch_dir, t)
+        align_img_dir = os.path.join(align_normal_img_dir, t)
+        sketch_imgs = os.listdir(sketch_img_dir)  # 0.png
+        for sketch_img in sketch_imgs:
+            try:
+                normal_img_name = sketch_img.split('.')[0] + '_normals.png'
+                sketch_img_path = os.path.join(sketch_img_dir, sketch_img)
+                align_img_path = os.path.join(align_img_dir, normal_img_name)
+                output_path = os.path.join(output_dir, t, sketch_img)
+                correct_sketch_img(sketch_img_path, align_img_path, output_path)
+            except Exception as e:
+                print(f"Error processing {t}: {e}")
+                continue
+    print('finish')
+
+    
+
+
+    # correct_sketch_img(sketch_img_path, align_normal_img_path)
     
 
 def correct_normal_bg_img(obj_img_path, output_img_path):
@@ -52,9 +99,9 @@ def correct_normal_bg_dataset():
 
 
 def main():
-
-    correct_normal_bg_dataset()
+    correct_sketch_dataset()
 
 
 if __name__ == '__main__':
     main()
+    # 00584159
--- utils/file_util.py
+++ utils/file_util.py
@@ -6,20 +6,19 @@ import cv2
 import numpy as np
 import trimesh
 import argparse
+import shutil
 
 
 def compare_sets(set1, set2):
-    keys1 = set(set1.keys())
-    keys2 = set(set2.keys())
 
-    only_in_pkl1 = keys1 - keys2
-    only_in_pkl2 = keys2 - keys1
+    only_in_pkl1 = set1 - set2
+    only_in_pkl2 = set2 - set1
 
-    print("文件名只在 pkl1 中存在：")
+    print("文件名只在 set1 中存在：")
     for name in sorted(only_in_pkl1):
         print("  ", name)
 
-    print("\n文件名只在 pkl2 中存在：")
+    print("\n文件名只在 set2 中存在：")
     for name in sorted(only_in_pkl2):
         print("  ", name)
 
@@ -28,106 +27,39 @@ def get_set_from_pkl(pkl_path):
     with open(pkl_path, 'rb') as f:
         data = pickle.load(f)
     keys = set(data.keys())
+    print(f"pkl键数量:{len(keys)}")
     return keys
 
 
 def get_set_from_dir(dir_path, file_type=None):
     files = os.listdir(dir_path)
     file_names = {f"{f.split('.')[0]}" for f in files}
+    print(f"路径中文件数量:{len(file_names)}")
     return file_names
 
 
-def get_pkl(pkl_path):
-    with open(pkl_path, 'rb') as f:
-        data = pickle.load(f)
-    any_key = next(iter(data))
-    print(data['00020346'])
+def rm_dir_without_file(dir_path):
+    files = os.listdir(dir_path)
+    if len(files) == 0:
+        os.rmdir(dir_path)
+        print(f"删除空目录: {dir_path}")
 
 
-def compare_keys(pkl1, pkl2):
-    with open(pkl1, 'rb') as f:
-        data1 = pickle.load(f)
-    with open(pkl2, 'rb') as f:
-        data2 = pickle.load(f)
+def rm_dir(dir1, dir2):
+    # 获取目录1和目录2中的所有文件名
+    files1 = set(os.listdir(dir1))
+    files2 = set(os.listdir(dir2))
 
-    keys1 = set(data1.keys())
-    keys2 = set(data2.keys())
+    # 找到在目录1中但不在目录2中的文件
+    only_in_dir1 = files1 - files2
 
-    only_in_pkl1 = keys1 - keys2
-    only_in_pkl2 = keys2 - keys1
-
-    print("文件名只在 pkl1 中存在：")
-    for name in sorted(only_in_pkl1):
-        print("  ", name)
-
-    print("\n文件名只在 pkl2 中存在：")
-    for name in sorted(only_in_pkl2):
-        print("  ", name)
+    # 删除这些文件
+    for file_name in only_in_dir1:
+        file_path = os.path.join(dir1, file_name)
+        shutil.rmtree(file_path)
+        print(f"删除文件: {file_path}")
 
 
-def compare_keys_and_files(pkl_path, dir_path):
-    with open(pkl_path, 'rb') as f:
-        data = pickle.load(f)
-    pkl_keys = set(data.keys())
-    dir_files = {f"{f.split('.')[0]}" for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))}
-    only_in_pkl = pkl_keys - dir_files  # pkl比文件多
-    only_in_dir = dir_files - pkl_keys
-
-    # print("文件名只在 pkl 中存在：")
-    for name in sorted(only_in_pkl):
-        # print("  ", name)
-        del data[name]
-
-    print(len(data))
-    with open('/home/lkh/siga/dataset/deepcad/data/cad_ply/views_correct.pkl', 'wb') as f:
-        pickle.dump(data, f)
-
-
-    # print("\n文件名只在目录中存在：")
-    # for name in sorted(only_in_dir):
-        # print("  ", name)
-
-
-
-def compare_filenames(path_a, path_b):
-    # 对比文件夹中的文件
-    files_a = set(os.listdir(path_a))
-    files_b = set(os.listdir(path_b))
-
-    files_a = {f for f in files_a if os.path.isfile(os.path.join(path_a, f))}
-    files_b = {f for f in files_b if os.path.isfile(os.path.join(path_b, f))}
-
-    only_in_a = files_a - files_b
-    only_in_b = files_b - files_a
-
-    print(f"仅在 {path_a} 中的文件:")
-    for f in sorted(only_in_a):
-        print("  ", f)
-
-    print(f"\n仅在 {path_b} 中的文件:")
-    for f in sorted(only_in_b):
-        print("  ", f)
-
-
-def read_h5(file):
-    with h5py.File(file, 'r') as f:
-        print("文件中的对象:")
-        def print_attrs(name, obj):
-            print(name)
-            for key, val in obj.attrs.items():
-                print(f"    {key}: {val}")
-        f.visititems(print_attrs)
-
-
-def read_normal_imgs_h5(file):
-    with h5py.File(file, 'r') as f:
-        normals = f['normals'] 
-    
-    # 检查是否有附加属性（如单位、描述等）
-    if normals.attrs:  # 如果有属性
-        print("附加属性:")
-        for key, value in normals.attrs.items():
-            print(f"  {key}: {value}")
 
 
 def merge_imgs(imgs_dir):
@@ -170,20 +102,16 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    # path_a = "/home/lkh/siga/dataset/deepcad/data/cad_ply/body2/base"
-    # path_b = "/home/lkh/siga/dataset/deepcad/data/cad_ply/body2/operate"
-    # path_c = "/home/lkh/siga/dataset/deepcad/data/cad_ply/body2/result"
-    # path_d = "/home/lkh/siga/dataset/deepcad/data/cad_ply/views_correct.pkl"
 
-    # compare_filenames(path_a, path_c)
-    # get_pkl(path_d)
-    # compare_keys_and_files(path_d, path_c)
-
-    # h5_dir = '/home/lkh/siga/CADIMG/output'
-    # h5_files = os.listdir(h5_dir)
-    # for i, file in enumerate(h5_files):
-    #     h5_path = os.path.join(h5_dir, file)
-    #     read_normal_imgs_h5(h5_path)
-    # from_ply_get_box()
-    merge_imgs(args.path)
-    # get_pkl('/home/lkh/siga/CADIMG/datasets/render_normal/centers.pkl')
+    # merge_imgs(args.path)
+    # p = '/home/lkh/siga/dataset/my_dataset/normals_train_dataset/normal_img_addbody/result'
+    # paths = os.listdir(p)
+    # for path in paths:
+    #     path_dir = os.path.join(p, path)
+    #     rm_dir_without_file(path_dir)
+    path1 = '/home/lkh/siga/dataset/my_dataset/normals_train_dataset/normal_img_addbody/base'
+    path2 = '/home/lkh/siga/dataset/my_dataset/normals_train_dataset/init_img_sketch'
+    # rm_dir(path2, path1)
+    a = get_set_from_dir(path1)
+    b = get_set_from_dir(path2)
+    compare_sets(a,b)
