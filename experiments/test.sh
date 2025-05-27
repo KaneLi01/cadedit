32828d7bda0245709468d22315d3521b2f763bd7
KaneLi01 <dstny6102@gmail.com>
2025-05-27 16:01:35 +0800

    test different devices

--- experiments/test.sh
+++ experiments/test.sh
@@ -1,7 +1,4 @@
-# for num in 2e-2 5e-3 
-# do
-#     python train_cn.py --weight_decay $num --batch_size 12 --lr 1e-5 --num_epochs 15 --torch_dtype float16
-# done
+
 
 DATA_DIR="/home/lkh/siga/dataset/my_dataset/cad_rgb_imgs/cad_controlnet_cube_light"
 BASE="/home/lkh/siga/dataset/my_dataset/cad_rgb_imgs/cad_controlnet_cube_light/test/base_img/007200.png"
