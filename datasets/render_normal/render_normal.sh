c4625874ad18b3cce416ee247f6b17d0341858c5
KaneLi01 <dstny6102@gmail.com>
2025-05-27 10:27:44 +0800

    mutli device sync

--- datasets/render_normal/render_normal.sh
+++
@@ -1,3 +0,0 @@
-
-
-blenderproc run render_normal.py --shape_type 'operate' --ply_name '00020346'
--- run.sh
+++
@@ -1,5 +0,0 @@
-for num in 2e-5 4e-5 5e-6 2e-6
-do
-    echo "正在执行 --lr $num"
-    python train_cn.py --lr $num
-done
