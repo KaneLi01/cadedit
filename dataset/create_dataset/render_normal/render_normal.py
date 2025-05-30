b3ff68c7a5a37333786bb4ba78ae279a828e99a8
KaneLi01 <dstny6102@gmail.com>
2025-05-30 17:41:42 +0800

    选择6视图渲染normals

--- dataset/create_dataset/render_normal/render_normal.py
+++ dataset/create_dataset/render_normal/render_normal.py
@@ -5,6 +5,8 @@ import trimesh
 import pickle
 import os
 import argparse
+from math import sqrt
+import json
 
 
 VIEW_CORNERS = [
@@ -17,6 +19,32 @@ VIEW_CORNERS = [
     [-2.0,  2.0, -2.0],
     [-2.0,  2.0,  2.0],
 ]
+
+VIEW_CORNERS_6 = {
+    'front': [ # front
+        [2.0,-2*sqrt(2),0.0], [2.0,-2.0,2.0], [2.0,0.0,2*sqrt(2)], [2.0,2.0,2.0], [2.0,2*sqrt(2),0.0], [2.0,0.0,-2*sqrt(2)]  
+    ],
+
+    'back': [ # back
+        [-2.0,-2*sqrt(2),0.0], [-2.0,-2.0,2.0], [-2.0,0.0,2*sqrt(2)], [-2.0,2.0,2.0], [-2.0,2*sqrt(2),0.0], [-2.0,0.0,-2*sqrt(2)]
+    ],
+
+    'right': [ # right
+        [0.0,2.0,2*sqrt(2)], [2.0,2.0,2.0], [2*sqrt(2),2.0,0.0], [2.0,2.0,-2.0], [0.0,2.0,-2*sqrt(2)], [-2*sqrt(2),2.0,0.0]
+    ],
+    
+    'left': [ # left
+        [0.0,-2.0,2*sqrt(2)], [2.0,-2.0,2.0], [2*sqrt(2),-2.0,0.0], [2.0,-2.0,-2.0], [0.0,-2.0,-2*sqrt(2)], [-2*sqrt(2),-2.0,0.0]
+    ],
+
+    'up': [ # up
+        [0.0,-2*sqrt(2),2.0], [2.0,-2.0,2.0], [2*sqrt(2),0,2.0], [2.0,2.0,2.0], [0.0,2*sqrt(2),2.0], [-2*sqrt(2),0.0,2.0]
+    ],
+    
+    'down': [ # down
+        [0.0,-2*sqrt(2),-2.0], [2.0,-2.0,-2.0], [2*sqrt(2),0,-2.0], [2.0,2.0,-2.0], [0.0,2*sqrt(2),-2.0], [-2*sqrt(2),0.0,-2.0]
+    ]
+}
 
 
 VIEW = {
@@ -57,21 +85,38 @@ def get_args():
     parser = argparse.ArgumentParser("1")
     parser.add_argument('--shape_type', type=str, help='渲染对象类型')
     parser.add_argument('--ply_name', type=str, help='被渲染的ply文件名') 
+    parser.add_argument('--view_path', type=str, help='视角pkl文件') 
+    parser.add_argument('--center_path', type=str, help='中心pkl文件')
+    parser.add_argument('--ply_dir', type=str, help='点云文件')
+    parser.add_argument('--output_dir', type=str, help='保存路径')
+    parser.add_argument('--correct_index', type=str, help='正确的索引')
 
     args = parser.parse_args()
 
-    args.view_path = '/home/lkh/siga/CADIMG/datasets/render_normal/views_correct.pkl'  # 视角pkl文件
-    args.center_path = '/home/lkh/siga/CADIMG/datasets/render_normal/centers_correct.pkl'  # 中心pkl文件
-    args.ply_dir = '/home/lkh/siga/dataset/deepcad/data/cad_ply/body2'
-    args.output_dir = '/home/lkh/siga/dataset/deepcad/data/cad_img/normal_body2'  # 渲染结果保存路径
+    # args.view_path = '/home/lkh/siga/CADIMG/datasets/render_normal/views_correct.pkl'  # 视角pkl文件
+    # args.center_path = '/home/lkh/siga/CADIMG/datasets/render_normal/centers_correct.pkl'  # 中心pkl文件
+    # args.ply_dir = '/home/lkh/siga/dataset/deepcad/data/cad_ply/body2'
+    # args.output_dir = '/home/lkh/siga/dataset/deepcad/data/cad_img/normal_body2'  # 渲染结果保存路径
 
     return args
 
 
 def main():
     args = get_args()
+    with open(args.correct_index, 'r') as f:
+        data = json.load(f)
+    for item in data:
+        if item.get('explaination') == 'filter edge face':
+            matched_index = item['file_names']
+    if args.ply_name not in matched_index:
+        print(f'------------------{args.ply_name}')
+        return False
 
     bproc.init()
+    light = bproc.types.Light()
+    light.set_type("POINT")
+    light.set_location([5, -5, 5])
+    light.set_energy(1000)
 
     # 读取要渲染的对象
     ply_path = os.path.join(args.ply_dir, args.shape_type, args.ply_name+'.ply')    
@@ -82,11 +127,11 @@ def main():
     # 读取视角和中心
     view = get_pkl(args.view_path, args.ply_name)
     center = np.array(get_pkl(args.center_path, args.ply_name))
-    camera_po_index = VIEW[view]
+    camera_po = VIEW_CORNERS_6[view]
 
     # 添加相机
-    for i in camera_po_index:
-        camera_pos =  np.array(VIEW_CORNERS[i])
+    for v in camera_po:
+        camera_pos =  np.array(v)
         rotation = look_at_rotation(camera_pos, center)
         cam_pose = bproc.math.build_transformation_mat(camera_pos, rotation)
         bproc.camera.add_camera_pose(cam_pose)
--- dataset/create_dataset/render_normal/show_result.sh
+++ dataset/create_dataset/render_normal/show_result.sh
@@ -8,14 +8,16 @@ for file in "$PLY_DIR"/*.ply; do
     file_name=$(basename "$file" .ply)
     echo "Processing file: $file_name"
     for SHAPE_TYPE  in 'base' 'operate' 'result'; do
+    # 渲染三种不同形状
 
         DIR="${HDF5_DIR}${SHAPE_TYPE}/${file_name}/"
         OUTPUT_DIR="${IMG_OUTPUT_DIR}${SHAPE_TYPE}/${file_name}"
 
+        # 渲染h5文件
         blenderproc run render_normal.py --shape_type "${SHAPE_TYPE}" --ply_name "${file_name}"
+        # 从h5文件中渲染图片
+        # blenderproc vis hdf5 "${DIR}0.hdf5" "${DIR}1.hdf5" "${DIR}2.hdf5"  "${DIR}3.hdf5" --keys "normals" --save "${OUTPUT_DIR}"
 
-        blenderproc vis hdf5 "${DIR}0.hdf5" "${DIR}1.hdf5" "${DIR}2.hdf5"  "${DIR}3.hdf5" --keys "normals" --save "${OUTPUT_DIR}"
-
-        python /home/lkh/siga/CADIMG/utils/file_util.py --path "${OUTPUT_DIR}"
+        # python /home/lkh/siga/CADIMG/utils/file_util.py --path "${OUTPUT_DIR}"
     done
 done
